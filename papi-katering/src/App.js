1c6ca7235be78a4457b54baca9264b6c34429b4e
kevinbennetth <kevinbennott@gmail.com>
2022-04-30 14:35:24 +0700

    ADD: login and register page

--- papi-katering/src/App.js
+++ papi-katering/src/App.js
@@ -1,10 +1,23 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                
-            </header>
-        </div>
+        <Router>
+            <div className="App">
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/login">
+                            <LoginPage />
+                        </Route>
+                        <Route exact path="/register">
+                            <RegisterPage />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
     );
 }
 
--- papi-katering/tailwind.config.js
+++ papi-katering/tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   content: [
     "src/index.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#28937F'
+      }
+    },
   },
   plugins: [],
 }
